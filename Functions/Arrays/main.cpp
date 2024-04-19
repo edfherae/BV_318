072aae1a76eff6f43220aa72e9fc36c556a936f7
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2024-04-19 18:47:52 +0300

    Templatisation DONE

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -4,44 +4,43 @@ using namespace std;
 
 #define tab "\t"
 
-void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+template<typename T>
+void FillRand(T arr[], const int n, int minRand = 0, int maxRand = 100);
 
-void Print(const int arr[], const int n);
-void Print(const double arr[], const int n);
+template<typename T>
+void Print(const T arr[], const int n);
 
-int  Sum(const int arr[], const int n);
-double Avg(const int arr[], const int n);
+template<typename T>
+T Sum(const T arr[], const int n);
 
-void Search(int arr[], const int n);
+template<typename T>
+T Avg(const T arr[], const int n);
+
+template<typename T>
+void Search(const T arr[], const int& n);
 
 int main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 50;
+	const int n = 10;
 	int arr[n];
-
-	/*int minRand, maxRand;
-	do
-	{
-		cout << "Введите минимально возможное случайное число: "; cin >> minRand;
-		cout << "Введите максимально возможное случайное число: "; cin >> maxRand;
-		if (minRand == maxRand)cout << "Пределы диапазона не должны совпадать" << endl;
-	} while (minRand == maxRand);*/
 
 	FillRand(arr, n);
 	Print(arr, n);
-	Search(arr, n);
-	/*cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	//Search(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
 
-	const int m = 8;
-	double brr[m];
-	FillRand(brr, m);
-	Print(brr, m);*/
+	const int D_SIZE = 10;
+	double d_arr[D_SIZE];
+	FillRand(d_arr, D_SIZE);
+	Print(d_arr, D_SIZE);
+
+	cout << "Сумма элементов массива: " << Sum(d_arr, D_SIZE) << endl;
 }
 
-void FillRand(int arr[], const int n, int minRand, int maxRand)
+template<typename T>
+void FillRand(T arr[], const int n, int minRand, int maxRand)
 {
 	if (maxRand < minRand)
 	{
@@ -58,37 +57,9 @@ void FillRand(int arr[], const int n, int minRand, int maxRand)
 		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
 	}
 }
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	//Заполнение массива случайными числами:
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minRand + rand() % (maxRand - minRand);
-		arr[i] /= 100;
-		//arr[i] = 50 + rand() % 50;
-		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
-		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
-	}
-}
 
-void Print(const int arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-void Print(const double arr[], const int n)
+template<typename T>
+void Print(const T arr[], const int n)
 {
 	//Вывод массива на экран:
 	for (int i = 0; i < n; i++)
@@ -98,52 +69,94 @@ void Print(const double arr[], const int n)
 	cout << endl;
 }
 
-int  Sum(const int arr[], const int n)
+template<typename T>
+T Sum(const T arr[], const int n)
 {
-	int sum = 0;
+	T sum = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += arr[i];
 	}
 	return sum;
 }
-double Avg(const int arr[], const int n)
+
+template<typename T>
+T Avg(const T arr[], const int n)
 {
-	return (double)Sum(arr, n) / n;
+	return (T)Sum(arr, n) / n;
 }
 
-void Search(int arr[], const int n)
-{
-	cout << "Повторяющиеся элементы: ";
-	int count = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (j != i && arr[j] == arr[i])
-			{
-				cout << arr[i] << tab;
-				count++;
-			}
-		}
-	}
-	cout << "\nКоличество повторений: " << count << endl;
-}
 
-void Search(int arr[], const int n)
-{
-	cout << "Повторяющиеся элементы: ";
-	int count = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (j != i && arr[j] == arr[i])
-			{
-				cout << arr[i] << tab;
-				count++;
-			}
-		}
-	}
-	cout << "\nКоличество повторений: " << count << endl;
-}
+
+//template<typename T>
+//void Search(const T arr[], const int& n)
+//{
+//	cout << "Повторяющиеся элементы: ";
+//	T temp[n];
+//	int count = 0, count_elem = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		//if элемент уже в temp, пропускаем
+//		bool is_elem_in_the_box = false;
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (arr[i] == temp[j])
+//			{
+//				is_elem_in_the_box = true;
+//			}
+//		}
+//		if (is_elem_in_the_box)
+//		{
+//			continue;
+//		}
+//		else
+//		{
+//			temp[i] = arr[i];
+//			for (int j = 0; j < n; j++)
+//			{
+//				if (j != i && arr[j] == arr[i])
+//				{
+//					cout << arr[i] << tab;
+//					count++;
+//				}
+//			}
+//		}
+//	}
+//	cout << "\nКоличество повторений: " << count << endl;
+//}
+
+//void Search(const int arr[], const int n)
+//{
+//	cout << "Повторяющиеся элементы: ";
+//	int temp[n];
+//	int count = 0, count_elem = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		//if элемент уже в temp, пропускаем
+//		bool is_elem_in_the_box = false;
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (arr[i] == temp[j])
+//			{
+//				is_elem_in_the_box = true;
+//			}
+//		}
+//		if (is_elem_in_the_box)
+//		{
+//			continue;
+//		}
+//		else
+//		{
+//			temp[i] = arr[i];
+//			for (int j = 0; j < n; j++)
+//			{
+//				if (j != i && arr[j] == arr[i])
+//				{
+//					cout << arr[i] << tab;
+//					count++;
+//				}
+//			}
+//		}
+//	}
+//	cout << "\nКоличество повторений: " << count << endl;
+//}
